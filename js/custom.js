2aa8623866ba7889bace07e4857502c2c03380e4
George Ramirez <george.ramirez2@gmail.com>
2018-10-01 15:17:43 -0600

    updates requested by Jim

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,7 @@
-let paramsP1 = "{name} the choices we make every day can help us live happier and healthier lives - both at {company name} and at home. That’s why you have been offered this exciting new diabetes health and wellness program called, LivingConnected from CCS Medical.";
-let paramsP2 = "LivingConnected is a new mobile health solution that gives you all the tools you need to start living better with diabetes. It is part of {company's} health benefit and it’s typically offered at $0 copay and no cost to you.";
+let paramsP1 = "{name} the choices we make everyday can help us live happier and healthier lives – both at {company name} and at home. That’s why you have been offered this exciting new diabetes benefit called, LivingConnected from CCS Medical.";
+
+let paramsP2 = "LivingConnected is an innovative mobile health solution that gives you the tools to start living better with diabetes. It is part of {company's} health benefits and its being offered at little or no cost to you.";
+
 
 //creates an object of all params and their values
 function getUrlVars() {
@@ -17,6 +19,3 @@ $(document).ready(()=> {
   $(".paragraph1").text(paramsP1.replace("{name}", `${params.name},`).replace("{company name}", params.company));
   $(".paragraph2").text(paramsP2.replace("{company's}", `${params.company}'s`))
 })
-
-
-
